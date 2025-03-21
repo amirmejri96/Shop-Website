e12f5a3016599055a97782a4d00fd080abee3e98
amirmejri96 <amirmejri1996@gmail.com>
2025-03-21 01:21:36 +0100

    avancement frontend product page

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -21,7 +21,7 @@ const ProductDisplay = (props) => {
             </div>
             <div className="productdisplay-right">
                 <h1> {product.name} </h1>
-                <div className="productdisplay-right-star">
+                <div className="productdisplay-right-stars">
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
                     <img src={star_icon} alt="" />
@@ -29,6 +29,30 @@ const ProductDisplay = (props) => {
                     <img src={start_dull_icon} alt="" />
                     <p>(122)</p>
                 </div>
+                <div className="productdisplay-right-prices">
+                    <div className="productdisplay-right-price-old">
+                        ${product.old_price}
+                    </div>
+                    <div className="productdisplay-right-price-new">
+                        ${product.new_price}
+                    </div>
+                </div>
+                <div className="productdisplay-right-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Quos aliquid reprehenderit tempora illo non! Reiciendis vero sint at quisquam illum?
+                </div>
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-sizes">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
+                    </div>
+                </div>
+                <button>ADD TO CART</button>
+                <p className="productdisplay-right-category"><span>Category :</span> Women, T-Shirt, Crop Top</p>
+                <p className="productdisplay-right-category"><span>Tags :</span> Modern, Latest</p>
             </div>
         </div>
     )
